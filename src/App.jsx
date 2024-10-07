b8882a98f4dedb572e5ac0b3fdcd2d50e53e7275
Nishu Paisa <gaoriverma6500@gmail.com>
2024-10-07 11:27:15 +0530

    fech from data from database

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,51 @@
-import { useState, useRef } from "react";
+import { useState, useRef , useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [alertstatus, setAlertStatus] = useState(false);
-  const [todo, setTodo] = useState(0);
+  const [Todos, setTodos] = useState([]);
 
+// fetching data from firebase
+
+useEffect(()=>{
+
+  fetch('https://tasktodos-2a764-default-rtdb.firebaseio.com/tasktodo.json').then((firebase_data)=>{
+    return firebase_data.json()}).then((firebase_data)=>{
+      console.log(firebase_data)
+      console.log("All the firebase data")
+      let tempTodos=[]
+        for(let key in firebase_data){
+       let currTodos={
+        id:key,
+        ...firebase_data[key]
+       }
+       tempTodos.push(currTodos);
+       console.log("tem function")
+      }
+      setTodos(tempTodos);
+      console.log("set todos function")
+    });
+
+},[])
+
+
+
+
+// upload data on firebase
   const title = useRef();
   const des = useRef();
 
   function addHandler() {
-    let todo = {
+    let currenttodo = {
       Title: title.current.value,
       Des: des.current.value,
     };
 
     fetch("https://tasktodos-2a764-default-rtdb.firebaseio.com/tasktodo.json", {
       method: "POST",
-      body: JSON.stringify(todo),
+      body: JSON.stringify(currenttodo),
     }).then(() => {
       setAlertStatus(true);
     });
@@ -38,7 +65,7 @@ function closeHandler(){
           width="18"
           height="18"
           fill="currentColor"
-          class="bi bi-x"
+          className="bi bi-x"
           viewBox="0 0 16 16"
         >
           <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
